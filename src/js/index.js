de40ac1e88f37cb885dd377e59a59a2b558998da
Elisa Ordenador M <reeallysong@gmail.com>
2023-01-29 17:20:12 -0100

    29.01.23

--- src/js/index.js
+++ src/js/index.js
@@ -2,4 +2,24 @@
 import '../scss/styles.scss';
 import { sayHello } from './demo.js';
 
+const cards = document.querySelectorAll('.card')
+
 sayHello();
+
+const starGame = allCards => {
+    allCards.forEach(card => card.classList.add('card--show'))
+}
+
+const starGameSecondPhase = allCards => {
+    allCards.forEach(card => card.classList.remove('card--show'))
+}
+
+const timeoutStarGame = setTimeout(() => {
+    starGame(cards)
+    clearTimeout(timeout)
+}, 100)
+
+const timeoutSecond = setTimeout(() => {
+    starGameSecondPhase(cards)
+    clearTimeout(timeout)
+}, 3000)
