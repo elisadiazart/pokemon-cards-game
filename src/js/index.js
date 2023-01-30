9d3ffffc1230ae94f5ce14e3db26a4c4abae10df
Elisa <elisadiazartcontact@gmail.com>
2023-01-31 00:05:40 +0100

    30.01.23 points html added

--- src/js/index.js
+++ src/js/index.js
@@ -103,5 +103,7 @@ const checkCards = () => {
 };
 
 gameBoard.addEventListener('click', e => {
+  console.log(e.target)
+  if(e.target.classList.contains('cards-board')) return
   showCards(e);
 });
